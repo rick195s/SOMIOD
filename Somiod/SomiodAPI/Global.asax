7d8e65306a6a018a7ba7dcb763e48aae724fc255
Pedro Braz <pedrobraz2002@gmail.com>
2022-11-29 10:07:36 +0000

    Creation of Models

---
+++ Somiod/SomiodAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SomiodAPI.WebApiApplication" Language="C#" %>
---
+++ Somiod/SomiodAPI/Models/subscription.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SomiodAPI
+{
+    public class subscription
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Creation_dt { get; set; }
+        public int Parent { get; set; }
+        public string Event { get; set; }
+        public string Endpoint { get; set; }
+    }
+}
