a02d1aa38b23d49f8f2f7c14a654cf695e1f9ad2
Ricardo Franco <ricardosf195@gmail.com>
2022-12-06 10:58:36 +0000

    sql helper for applications

--- SomiodAPI/Controllers/ModuleController.cs
+++ SomiodAPI/Controllers/ModuleController.cs
@@ -17,101 +17,14 @@ namespace SomiodAPI.Controllers
         
         string connectionString = SomiodAPI.Properties.Settings.Default.connStr;
 
-        // GET: api/somiod/module
-        [Route("module")]
-        public IEnumerable<Module> GetAllModules()
-        {
-            List<Module> modules = new List<Module>();
-            SqlConnection conn = null;
-            string sql = "SELECT * FROM Module ORDER BY Id";
-
-            try
-            {
-                conn = new SqlConnection(connectionString);
-                conn.Open();
-                SqlCommand cmd = new SqlCommand(sql, conn);
-
-
-                SqlDataReader reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    Module module = new Module
-                    {
-                        Id = (int)reader["Id"],
-                        Name = (string)reader["Name"],
-                        Creation_dt = (string)reader["Creation_dt"],
-                        Parent = (int)reader["Parent"]
-                    };
-
-                    modules.Add(module);
-                }
-                reader.Close();
-                conn.Close();
-            }
-            catch (Exception)
-            {
-                //fechar a ligação à BD
-                if (conn.State == System.Data.ConnectionState.Open)
-                {
-                    conn.Close();
-                }
-            }
-            return modules;
-        }
-
-        // GET: api/somiod/module/5
-        [Route("module/{id}")]
-        public IHttpActionResult GetModule(int id)
-        {
-            Module module = null;
-            SqlConnection conn = null;
-
-            try
-            {
-                conn = new SqlConnection(connectionString);
-                conn.Open();
-
-                string sql = "SELECT * FROM Module WHERE Id=@idApp";
-                SqlCommand cmd = new SqlCommand(sql, conn);
-                cmd.Parameters.AddWithValue("@idApp", id);
-
-                SqlDataReader reader = cmd.ExecuteReader();
-                if (reader.Read())
-                {
-                    module = new Module
-                    {
-                        Id = (int)reader["Id"],
-                        Name = (string)reader["Name"],
-                        Creation_dt = (string)reader["Creation_dt"],
-                        Parent = (int)reader["Parent"]
-                    };
-                }
-                reader.Close();
-                conn.Close();
-                if (module == null)
-                {
-                    return NotFound();
-                }
-                return Ok(module);
-
-            }
-            catch (Exception)
-            {
-                //fechar a ligação à BD
-                if (conn.State == System.Data.ConnectionState.Open)
-                {
-                    conn.Close();
-                }
-                return NotFound();
-            }
-        }
 
         // POST: api/somiod/Lamp
         [Route("{applicationName}")]
         public IHttpActionResult PostModule([FromBody]Module value, string applicationName)
         {
             SqlConnection conn = null;
-            value.Parent = 0;
+            
+            return Ok();
 
 
             try
@@ -163,78 +76,6 @@ namespace SomiodAPI.Controllers
             }
         }
 
-        // PUT: api/somiod/module/5
-        [Route("module/{id}")]
-        public IHttpActionResult PutModule(int id, [FromBody]Module value)
-        {
-            SqlConnection conn = null;
-
-            try
-            {
-                conn = new SqlConnection(connectionString);
-                conn.Open();
-
-                string sql = "UPDATE Module SET Name=@Name, Creation_dt=@Creation WHERE Id=@Id";
-                SqlCommand cmd = new SqlCommand(sql, conn);
-                cmd.Parameters.AddWithValue("@Name", value.Name);
-                cmd.Parameters.AddWithValue("@Creation", value.Creation_dt);
-                cmd.Parameters.AddWithValue("@Id", id);
-
-                int numRows = cmd.ExecuteNonQuery();
-                conn.Close();
-
-                if (numRows > 0)
-                {
-                    return Ok();
-                }
-                return InternalServerError();
-
-            }
-            catch (Exception)
-            {
-                //fechar a ligação à BD
-                if (conn.State == System.Data.ConnectionState.Open)
-                {
-                    conn.Close();
-                }
-                return InternalServerError();
-            }
-        }
-
-        // DELETE: api/somiod/module/5
-        [Route("module/{id}")]
-        public IHttpActionResult DeleteModule(int id)
-        {
-            SqlConnection conn = null;
-
-            try
-            {
-                conn = new SqlConnection(connectionString);
-                conn.Open();
-
-                string sql = "DELETE FROM Module WHERE Id=@Id";
-                SqlCommand cmd = new SqlCommand(sql, conn);
-                cmd.Parameters.AddWithValue("@Id", id);
-
-                int numRows = cmd.ExecuteNonQuery();
-                conn.Close();
-
-                if (numRows > 0)
-                {
-                    return Ok();
-                }
-                return InternalServerError();
-
-            }
-            catch (Exception)
-            {
-                //fechar a ligação à BD
-                if (conn.State == System.Data.ConnectionState.Open)
-                {
-                    conn.Close();
-                }
-                return InternalServerError();
-            }
-        }
+     
     }
 }
---
+++ SomiodAPI/SqlServerHelper.cs
@@ -0,0 +1,140 @@
+﻿using SomiodAPI;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace AuthService
+{
+
+    /// <summary>
+    /// Summary description for SqlServerHelper
+    /// </summary>
+    public class SqlServerHelper
+    {
+
+        static string connectionString = SomiodAPI.Properties.Settings.Default.connStr;
+
+        public static int ApplicationExists(string application)
+        {
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            try
+            {
+             
+                SqlCommand cmd = new SqlCommand();
+
+                // attention...
+                cmd.CommandText = "SELECT id FROM Applications where name = @name";
+                cmd.Parameters.AddWithValue("name", application);
+    
+
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = sqlConnection;
+                sqlConnection.Open();
+
+                int id = (int)cmd.ExecuteScalar();
+                return id;
+            }
+            catch
+            {
+                return 0;
+            }
+            finally
+            {
+                if (sqlConnection != null)
+                    sqlConnection.Close();
+            }
+        }
+
+
+        public static Application GetApplication(int id)
+        {
+            SqlConnection sqlConnection = null;
+            try
+            {
+                sqlConnection = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand();
+                SqlDataReader reader;
+
+                // isto DEVE que ser alterado .... para usar SQLParameters
+                cmd.CommandText = "SELECT * FROM Application where id = @id";
+                cmd.Parameters.AddWithValue("id", id);
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = sqlConnection;
+                sqlConnection.Open();
+
+                reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    Application application = LoadApplication(reader);
+                    return application;
+                }
+
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                if (sqlConnection != null)
+                    sqlConnection.Close();
+            }
+        }
+
+        public static List<Application> GetApplications()
+        {
+            List<Application> applications = new List<Application>();
+            SqlConnection sqlConnection = null;
+            try
+            {
+                sqlConnection = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand();
+                SqlDataReader reader;
+
+                cmd.CommandText = "SELECT * FROM Applications";
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = sqlConnection;
+                sqlConnection.Open();
+
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Application application = LoadApplication(reader);
+                    applications.Add(application);
+                }
+                return applications;
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                if (sqlConnection != null)
+                    sqlConnection.Close();
+            }
+        }
+            
+
+        private static Application LoadApplication(SqlDataReader reader)
+        {
+            Application application = new Application();
+
+            application.Id = reader.GetSqlInt32(reader.GetOrdinal("Id")).Value;
+            application.Name = reader.GetString(reader.GetOrdinal("Name"));
+            application.Creation_dt = reader.GetString(reader.GetOrdinal("Creation_dt"));
+            
+            return application;
+        }
+
+    }
+}
