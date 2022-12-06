0555e7bf9a29b92b3700a723c76b1e1a898c30a8
Pedro Braz <pedrobraz2002@gmail.com>
2022-12-06 11:04:15 +0000

    Implemented POST Data or Subscription and DELETE subscription

--- SomiodAPI/Controllers/SomiodController.cs
+++ SomiodAPI/Controllers/SomiodController.cs
@@ -286,6 +286,95 @@ namespace SomiodAPI.Controllers
                 return InternalServerError();
             }
         }
+
+        // POST: api/somiod/Lamp/Light
+        [Route("{applicationName}/{moduleName}")]
+        public IHttpActionResult PostSubscription_Data([FromBody] Subscription value, string applicationName, string moduleName)
+        {
+            SqlConnection conn = null;
+            int applicationId = 0;
+            value.Parent = 0;
+            
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+
+                string sql2 = "SELECT * FROM Application WHERE Name=@ApplicationName";
+                SqlCommand cmd2 = new SqlCommand(sql2, conn);
+                cmd2.Parameters.AddWithValue("@ApplicationName", applicationName);
+                SqlDataReader reader = cmd2.ExecuteReader();
+                while (reader.Read())
+                {
+                    applicationId = (int)reader["Id"];
+                }
+
+                Debug.WriteLine("id aplicação: " + applicationId);
+                reader.Close();
+
+                if (applicationId == 0)
+                {
+                    return InternalServerError();
+                }
+
+                string sql3 = "SELECT * FROM Module WHERE Name=@ModuleName";
+                SqlCommand cmd3 = new SqlCommand(sql3, conn);
+                cmd3.Parameters.AddWithValue("@ModuleName", moduleName);
+                SqlDataReader reader2 = cmd3.ExecuteReader();
+                while (reader2.Read())
+                {
+                    value.Parent = (int)reader2["Id"];
+                }
+
+                Debug.WriteLine("id modulo: " + value.Parent);
+                reader2.Close();
+
+                if (value.Parent == 0)
+                {
+                    return InternalServerError();
+                }
+
+                int numRows = 0;
+                if (value.Res_type.ToLower() == "subscription") { 
+                    string sql = "INSERT INTO Subscription VALUES(@Name, @Creation, @Parent, @Event, @Endpoint)";
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@Name", value.Name);
+                    cmd.Parameters.AddWithValue("@Creation", value.Creation_dt);
+                    cmd.Parameters.AddWithValue("@Parent", value.Parent);
+                    cmd.Parameters.AddWithValue("@Event", value.Event);
+                    cmd.Parameters.AddWithValue("@Endpoint", value.Endpoint);
+
+                    numRows = cmd.ExecuteNonQuery();
+                } else if (value.Res_type.ToLower() == "data")
+                {
+                    string sql = "INSERT INTO Data VALUES(@Name, @Creation, @Parent)";
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@Name", value.Name);
+                    cmd.Parameters.AddWithValue("@Creation", value.Creation_dt);
+                    cmd.Parameters.AddWithValue("@Parent", value.Parent);
+
+                    numRows = cmd.ExecuteNonQuery();
+                }
+
+
+                conn.Close();
+                if (numRows > 0)
+                {
+                    return Ok();
+                }
+                return InternalServerError();
+
+            }
+            catch (Exception)
+            {
+                //fechar a ligação à BD
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                return InternalServerError();
+            }
+        }
         #endregion
 
         #region PUT
@@ -415,6 +504,78 @@ namespace SomiodAPI.Controllers
                 conn.Open();
 
                 string sql = "DELETE FROM Module WHERE Id=@Id";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                int numRows = cmd.ExecuteNonQuery();
+                conn.Close();
+
+                if (numRows > 0)
+                {
+                    return Ok();
+                }
+                return InternalServerError();
+
+            }
+            catch (Exception)
+            {
+                //fechar a ligação à BD
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                return InternalServerError();
+            }
+        }
+
+        // DELETE: api/somiod/datas/5
+        [Route("datas/{id}")]
+        public IHttpActionResult DeleteData(int id)
+        {
+            SqlConnection conn = null;
+
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+
+                string sql = "DELETE FROM Data WHERE Id=@Id";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                int numRows = cmd.ExecuteNonQuery();
+                conn.Close();
+
+                if (numRows > 0)
+                {
+                    return Ok();
+                }
+                return InternalServerError();
+
+            }
+            catch (Exception)
+            {
+                //fechar a ligação à BD
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                return InternalServerError();
+            }
+        }
+
+        // DELETE: api/somiod/subscriptions/5
+        [Route("subscriptions/{id}")]
+        public IHttpActionResult DeleteSubscriptions(int id)
+        {
+            SqlConnection conn = null;
+
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+
+                string sql = "DELETE FROM Subscription WHERE Id=@Id";
                 SqlCommand cmd = new SqlCommand(sql, conn);
                 cmd.Parameters.AddWithValue("@Id", id);
 
